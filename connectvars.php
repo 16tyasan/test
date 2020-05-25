38be60e2264115613a7b804f58c98367ab28223a
16tyasan <cssrn747@yahoo.co.jp>
2020-05-25 17:25:15 +0900

    削除
    セキュリティのため

--- connectvars.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // Define database connection constants
-  define('DB_HOST', 'mysql904b.xserver.jp');
-  define('DB_USER', 'tokaina_mytest1');
-  define('DB_PASSWORD', 'Ys4andafrn');
-  define('DB_NAME', 'tokaina_riskyjobs');
-?>
